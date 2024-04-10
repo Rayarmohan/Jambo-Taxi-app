2122b0935db56f69d445a23471c0cfe5f0d42753
Rayarmohan <mohanmanu086@gmail.com>
2024-04-10 12:59:34 +0530

    Rating Screen

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         theme: AppTheme.appTheme,
         debugShowCheckedModeBanner: false,
         title: 'jambotaxiapp',
-        initialRoute: AppRoute.onboard,
+        initialRoute: AppRoute.rating,
         getPages: appRoutes(),
       ),
     );
