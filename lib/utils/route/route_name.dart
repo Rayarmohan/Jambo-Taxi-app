27b2d1e757c36944c6a0a3ef048850de71272bce
CatherinCRaphel <159515750+CatherinCRaphel@users.noreply.github.com>
2024-04-10 16:22:50 +0530

    ui of helpcenter-privacy-updatedocument-logout

--- lib/utils/route/route_name.dart
+++ lib/utils/route/route_name.dart
@@ -2,4 +2,7 @@ class AppRoute {
   static String onboard = '/OnBoardScreen';
   static String login = '/LoginScreen';
   static String otpscreen = '/OtpScreen';
+  static String helpcenter = '/HelpCenter';
+  static String privacy = '/PrivacyScreen';
+  static String updatedocument = '/UpdateDocument';
 }
