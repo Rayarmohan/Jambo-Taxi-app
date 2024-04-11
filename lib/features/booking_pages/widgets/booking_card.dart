a32e20cf60dfb5b6cc38f4ecd776100b5a135c0e
helanaa123 <helana.acube@gmail.com>
2024-04-11 17:30:45 +0530

    first commit

--- lib/features/booking_pages/widgets/booking_card.dart
+++ lib/features/booking_pages/widgets/booking_card.dart
@@ -1,44 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:jambotaxi/features/booking_pages/widgets/vertical_line_painter.dart';
 import '../../../utils/color/app_colors.dart';
 
 class BookingCard extends StatelessWidget {
   final String statusText;
   const BookingCard({
-    Key? key,
+    super.key,
     required this.statusText,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
       child: Card(
         elevation: 4,
         child: Column(
-          mainAxisSize: MainAxisSize.min, // Add this line
+          mainAxisAlignment: MainAxisAlignment.start,// Add this line
           children: [
             ListTile(
-              leading: const CircleAvatar(
-                radius: 40,
-                backgroundImage: AssetImage(
-                    'assets/images/profile.png'), // Your profile image
+              leading: const Padding(
+                padding: EdgeInsets.only(top: 8.0,),
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage(
+
+                      'assets/images/profile.png'), // Your profile image
+                ),
               ),
-              title: Text('Jessica'),
-              subtitle: Text('CRN: #854HG23'),
+              title: const Text('Jessica'),
+              subtitle: const Text('CRN: #854HG23'),
               trailing: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Container(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    decoration: BoxDecoration(
+                    padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 2),
+                    decoration: const BoxDecoration(
                       color: AppColors.primeryColor,
-                      borderRadius: BorderRadius.circular(20),
+                      borderRadius: BorderRadius.zero,
                     ),
                     child: Text(
                       statusText,
-                      style: TextStyle(color: AppColors.white),
+                      style: const TextStyle(color: AppColors.white,
+                      fontSize: 10),
                     ),
                   ),
+                  const SizedBox(height: 8.0,),
                   const Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -52,6 +59,7 @@ class BookingCard extends StatelessWidget {
             const Divider(),
             const Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 IconText(icon: Icons.location_on, text: '4.5 mile'),
                 IconText(icon: Icons.access_time, text: '4 mins'),
@@ -69,32 +77,46 @@ class BookingCard extends StatelessWidget {
             ),
             const Divider(),
             const SizedBox(height: 8.0),
-            const Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Row(
+                SizedBox(width: 20.0),
+                Column(
                   children: [
-                    SizedBox(width: 20.0),
-                    Icon(Icons.my_location_outlined,
+                    const SizedBox(width: 50.0),
+                    const Icon(Icons.my_location_outlined,
                         color: AppColors.primeryColor),
-                    SizedBox(width: 20),
-                    Text('219 Florida Rd, Durban'),
+                    SizedBox(
+                      height: 15,
+                      child: CustomPaint(
+                        painter: VerticalLinePainter(color: AppColors.primeryColor, thickness: 1),
+                      ),
+                    ),
+                    const Icon(Icons.location_on, color: AppColors.primeryColor),
                   ],
                 ),
-                Divider(),
-                Row(
+
+                SizedBox(width: 20.0),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(width: 20.0),
-                    Icon(Icons.location_on, color: AppColors.primeryColor),
-                    SizedBox(width: 20.0),
+                    Text('219 Florida Rd, Durban'),
+                    Divider(color: AppColors.grey,),
+                    SizedBox(height: 10.0),
                     Text('KwaZulu-Natal, Cape Town'),
                   ],
                 ),
               ],
             ),
-            const SizedBox(height: 8.0),
+            const SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero, // Border radius
+                ),
+              ),
               child:   Row(
                 children: [
                   Text('Booking car type',
@@ -102,19 +124,16 @@ class BookingCard extends StatelessWidget {
                       .textTheme
                       .titleSmall!
                       .copyWith(height: 1.7, color: AppColors.white)),
-                  Spacer(),
+                  const SizedBox(
+                    width: 18.5,
+                  ),
                   Text('Sedan',
                       style:Theme.of(context)
                           .textTheme
                           .titleMedium!
                           .copyWith(height: 1.7, color: AppColors.white)),
-                  Icon(Icons.chevron_right),
+                  const Icon(Icons.chevron_right),
                 ],
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero, // Border radius
-                ),
               ),
             ),
           ],
@@ -129,17 +148,17 @@ class IconText extends StatelessWidget {
   final String text;
 
   const IconText({
-    Key? key,
+    super.key,
     required this.icon,
     required this.text,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Icon(icon, size: 30.0, color: AppColors.primeryColor),
-        SizedBox(width: 4.0),
+        const SizedBox(width: 4.0),
         Text(text),
       ],
     );
